5cd75c01c0d5bc45e175703b134e76f4a0951aef
Cody B Duncan <codybstrange93@gmail.com>
2025-01-25 11:53:14 +0100

    Add rotation and player update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
-
+from circleshape import *
+from player import *
 
 def main():
     pygame.init()
@@ -8,13 +9,15 @@ def main():
     # FPS
     clock = pygame.time.Clock()
     dt = 0
-
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
--- player.py
+++ player.py
@@ -1,9 +1,21 @@
-from constants import PLAYER_RADIUS
+from constants import *
+from circleshape import CircleShape
+import pygame
 
-def Player(CircleShape):
+class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+
+    def rotate(self, dt):
+        self.rotation += dt * PLAYER_TURN_SPEED
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -12,3 +24,6 @@ def Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a,b,c]
+    def draw(self, screen):
+        line_width = 2
+        pygame.draw.polygon(screen, (255, 255, 255), self.triangle(), line_width)
